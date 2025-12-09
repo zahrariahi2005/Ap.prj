d59b2b8b4a72e378701cc92090547d865b753115
sabaghasemi2006bmw <sabaghasemi08@gmail.com>
2025-12-10 00:15:10 +0330

    I have added Events.h

---
+++ Events.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include<vector>
+class Participant;
+class Events {
+	string name;
+	int id;
+	string type;
+	int capacity;
+	vector<Participant*>Pr;
+public:
+	Events(string name, int id, string type, int capacity);
+	string getname();
+	int getid()const;
+	string gettype();
+	int getcapacity();
+	int getParticipantCount() ;
+	void addParticipant(Participant* participant);
+	const vector<Participant*>& getParticipants() const;
+};
