50079bc72874298acf402ca80c2d76c708380767
sabaghasemi2006bmw <sabaghasemi08@gmail.com>
2025-12-10 23:48:42 +0330

    I have added Manager.h

---
+++ Manager.h
@@ -0,0 +1,19 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#include<vector>
+#include"Events.h"
+
+class Manager {
+	vector<Events> events;        
+	vector<Participant> participants;
+public:
+	void addEvent(const Events& event);
+	void addParticipant(const Participant& participant);
+	Events* findEvent(int eventID);
+	Participant* findParticipant(int participantID);
+	void registerParticipantToEvent(int participantID, int eventID);
+	void listEventsSortedByType() const;
+	void listEventsSortedByParticipantCount() const;
+
+};
