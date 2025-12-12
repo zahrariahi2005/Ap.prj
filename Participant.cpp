fe4518f693b2599b2ab0f7667404f751addf0e30
sabaghasemi2006bmw <sabaghasemi08@gmail.com>
2025-12-12 22:38:44 +0330

    Update Participant.cpp

--- Participant.cpp
+++ Participant.cpp
@@ -1,12 +1,10 @@
 #include "Participant.h"
 using namespace std;
-// Constructor
 Participant::Participant(string n, int i) {
     name = n;
     id = i;
 }
 
-// Getters
 string Participant::getName() const {
     return name;
 }
@@ -15,13 +13,12 @@ int Participant::getid() const {
     return id;
 }
 
-// Add event
 void Participant::addEvent(int eventID) {
     registeredEvents.push_back(eventID);
 }
 
-// Get all registered events
 const vector<int>& Participant::getRegisteredEvents() const {
     return registeredEvents;
 
 }
+
