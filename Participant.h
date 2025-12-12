178fc53a0c5941c6acdb1d3e3273c3723e828eaa
sabaghasemi2006bmw <sabaghasemi08@gmail.com>
2025-12-12 22:34:21 +0330

    Update Participant.h

--- Participant.h
+++ Participant.h
@@ -1,38 +1,36 @@
-#ifndef PARTICIPANT_H
-#define PARTICIPANT_H
 
 #include <string>
 #include <vector>
 #include <iostream>
-
+using namespace std;
 class Participant {
 private:
-    std::string name;
-    int participantID;
-    std::vector<int> registeredEvents;
+    string name;
+    int i;
+    vector<int> registeredEvents;
 
 public:
     Participant() = default;
-    Participant(int id, const std::string &n) : name(n), participantID(id) {}
+    Participant(int id, const string &n) : name(n), id(id) {}
 
     int getid() const { return participantID; }
-    const std::string &getName() const { return name; }
-    const std::vector<int> &getRegisteredEvents() const { return registeredEvents; }
+    const string &getName() const { return name; }
+    const vector<int> &getRegisteredEvents() const { return registeredEvents; }
 
     void addEvent(int eventID) {
         registeredEvents.push_back(eventID);
     }
 
     bool isRegisteredTo(int eventID) const {
-        for (int id : registeredEvents) if (id == eventID) return true;
+        for (int e : registeredEvents) if (e == eventID) return true;
         return false;
     }
 
-    friend std::ostream &operator<<(std::ostream &os, const Participant &p) {
+    friend ostream &operator<<(ostream &os, const Participant &p) {
         os << "Participant[ID=" << p.participantID << ", Name='" << p.name << "', Events=" << p.registeredEvents.size() << "]";
         return os;
     }
 };
 
 
-#endif // PARTICIPANT_H
+
