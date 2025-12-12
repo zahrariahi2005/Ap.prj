5e09e1852882b7b50de8a3c772a4dcff2b930180
sabaghasemi2006bmw <sabaghasemi08@gmail.com>
2025-12-12 22:55:59 +0330

    Update Manager.h

--- Manager.h
+++ Manager.h
@@ -8,6 +8,8 @@ class Manager {
 	vector<Events> events;        
 	vector<Participant> participants;
 public:
+     void printAllEvents() const;
+    void printAllParticipants() const;
 	void addEvent(const Events& event);
 	void addParticipant(const Participant& participant);
 	Events* findEvent(int eventID);
